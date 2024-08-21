f6a215ec17c674d04c87bc2c1bbdd992033d3018
arsenok <arsenok@greetingsisland.com>
2024-08-21 14:08:47 +0300

    issue example

--- src/js/capacitor-welcome.js
+++ src/js/capacitor-welcome.js
@@ -1,5 +1,7 @@
 import { Browser } from "@capacitor/browser";
 import { SplashScreen } from "@capacitor/splash-screen";
+import { Printer } from "@capawesome-team/capacitor-printer";
+import { Filesystem, Directory } from "@capacitor/filesystem";
 
 window.customElements.define(
   "capacitor-welcome",
@@ -25,6 +27,7 @@ window.customElements.define(
         value="https://capacitorjs.com/"
       />
       <button id="open-browser">Open Browser</button>
+      <button id="print">Print PDF</button>
     </main>
     `;
     }
@@ -40,6 +43,28 @@ window.customElements.define(
             return;
           }
           await Browser.open({ url: input });
+        });
+
+      self.shadowRoot
+        .querySelector("#print")
+        .addEventListener("click", async function (event) {
+          const minimalPDFBase64 = 'JVBERi0xLjEKJcKlwrHDqwoKMSAwIG9iagogIDw8IC9UeXBlIC9DYXRhbG9nCiAgICAgL1BhZ2VzIDIgMCBSCiAgPj4KZW5kb2JqCgoyIDAgb2JqCiAgPDwgL1R5cGUgL1BhZ2VzCiAgICAgL0tpZHMgWzMgMCBSXQogICAgIC9Db3VudCAxCiAgICAgL01lZGlhQm94IFswIDAgMzAwIDE0NF0KICA+PgplbmRvYmoKCjMgMCBvYmoKICA8PCAgL1R5cGUgL1BhZ2UKICAgICAgL1BhcmVudCAyIDAgUgogICAgICAvUmVzb3VyY2VzCiAgICAgICA8PCAvRm9udAogICAgICAgICAgIDw8IC9GMQogICAgICAgICAgICAgICA8PCAvVHlwZSAvRm9udAogICAgICAgICAgICAgICAgICAvU3VidHlwZSAvVHlwZTEKICAgICAgICAgICAgICAgICAgL0Jhc2VGb250IC9UaW1lcy1Sb21hbgogICAgICAgICAgICAgICA+PgogICAgICAgICAgID4+CiAgICAgICA+PgogICAgICAvQ29udGVudHMgNCAwIFIKICA+PgplbmRvYmoKCjQgMCBvYmoKICA8PCAvTGVuZ3RoIDU1ID4+CnN0cmVhbQogIEJUCiAgICAvRjEgMTggVGYKICAgIDAgMCBUZAogICAgKEhlbGxvIFdvcmxkKSBUagogIEVUCmVuZHN0cmVhbQplbmRvYmoKCnhyZWYKMCA1CjAwMDAwMDAwMDAgNjU1MzUgZiAKMDAwMDAwMDAxOCAwMDAwMCBuIAowMDAwMDAwMDc3IDAwMDAwIG4gCjAwMDAwMDAxNzggMDAwMDAgbiAKMDAwMDAwMDQ1NyAwMDAwMCBuIAp0cmFpbGVyCiAgPDwgIC9Sb290IDEgMCBSCiAgICAgIC9TaXplIDUKICA+PgpzdGFydHhyZWYKNTY1CiUlRU9GCg=='
+
+          const fileName = `My design_${new Date().getTime()}.pdf`;
+
+          const savedFileTemp = await Filesystem.writeFile({
+            path: fileName,
+            data: minimalPDFBase64,
+            directory: Directory.Cache,
+          });
+
+          await Printer.printPdf({
+            path: savedFileTemp.uri,
+          });
+
+          await Filesystem.deleteFile({
+            path: savedFileTemp.uri,
+          });
         });
     }
   },
